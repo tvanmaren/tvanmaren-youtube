04c46f1209e93a21b9256962081903f433b142dd
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-07-18 11:59:18 -0600

    :tada: setup & import project

---
+++ src/components/search_bar.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        Search Term:&nbsp;
+        <input placeholder="e.g., Lady Gaga"
+          value={this.state.term}
+          onChange={event=>this.onInputChange(event.target.value)} />
+      </div>
+    );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
+}
+
+export default SearchBar;
